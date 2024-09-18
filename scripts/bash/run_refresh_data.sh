2394915c3bdbcd58591a96f1808be93e9566c5e1
Edward Farragher <edward.farragher@outlook.com>
2024-09-18 21:26:53 +0100

    Adding git steps to shell script

--- scripts/bash/run_refresh_data.sh
+++ scripts/bash/run_refresh_data.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Pull the latest changes from the remote repository
+git pull
+
 # Set PYTHONPATH and run the Python script
 export PYTHONPATH=$(pwd)
 python scripts/python/refresh_data.py
+
+# Get the score GW
+gameweek=$(python -c "import json; import sys; print(json.load(open('data/scoring_meta.json'))['scoring_data_gameweek'])")
+
+# Add all changes to the staging area
+git add .
+
+# Commit changes with a dynamic message
+git commit -m "Rescoring for Gameweek $gameweek"
+
+# Push changes to the current branch
+git push
+
+#Print
+echo "Retrained for: Gameweek $gameweek"
